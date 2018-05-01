5bb925fd99d5f981499dd4674954d8c40f001254
Hans Markwart <hans@rapidrabbit.com>
2018-05-01 21:23:00 +0200

    - added recursive searching of view pager

--- library/src/main/java/com/sihamark/simpleviewpager/Binding.kt
+++ library/src/main/java/com/sihamark/simpleviewpager/Binding.kt
@@ -8,7 +8,29 @@ import android.view.View
 
 @BindingAdapter("viewPager")
 fun setViewPager(tabLayout: TabLayout, @IdRes viewPagerId: Int) {
-	val viewPager: ViewPager = (tabLayout.parent as View)
-		.findViewById(viewPagerId) ?: return
+	val viewPager = searchForViewPager(tabLayout, viewPagerId) ?: return
 	tabLayout.setupWithViewPager(viewPager)
+}
+
+private fun searchForViewPager(view: View, @IdRes viewPagerId: Int): ViewPager? {
+	return searchRecursively(view, viewPagerId, 0, 3)
+}
+
+private fun searchRecursively(
+	view: View, @IdRes viewPagerId: Int,
+	level: Int,
+	maxLevel: Int
+): ViewPager? {
+	if (level >= maxLevel) return null
+	val viewPager = view.findViewById<ViewPager>(viewPagerId)
+	return if (viewPager != null) {
+		viewPager
+	} else {
+		val parent = view.parent as? View
+		if (parent != null) {
+			searchRecursively(parent, viewPagerId, level + 1, maxLevel)
+		} else {
+			null
+		}
+	}
 }
