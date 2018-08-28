d4965902799d110d947bdf0fee7d3cc0c4dccc5e
Hans <hans@rapidrabbit.com>
2018-08-28 15:26:22 +0200

    - changed the pager adapter to inherit from FragmentStatePagerAdapter - updated android gradle plugin - updated kotlin

--- library/src/main/java/com/muellerwulff/simpleviewpager/ListPagerAdapter.kt
+++ library/src/main/java/com/muellerwulff/simpleviewpager/ListPagerAdapter.kt
@@ -6,12 +6,12 @@ package com.muellerwulff.simpleviewpager
 
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
-import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.app.FragmentStatePagerAdapter
 
 class ListPagerAdapter(
 	fragmentManager: FragmentManager,
 	private val pages: List<Page>
-) : FragmentPagerAdapter(fragmentManager) {
+) : FragmentStatePagerAdapter(fragmentManager) {
 	override fun getItem(position: Int): Fragment = pages[position].fragment
 	override fun getCount(): Int = pages.size
 	override fun getPageTitle(position: Int): CharSequence? = pages[position].title
